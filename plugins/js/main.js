adaa369a53646b994152251cbb32e92fc4bb56ee
Abdallah <abdullah.taha574@gmail.com>
2024-10-07 19:49:14 +0300

    Update on Header

--- plugins/js/main.js
+++ plugins/js/main.js
@@ -1,3 +1,5 @@
+
+// Function to send an email using EmailJS service
 async function sendMail(event) {
   event.preventDefault();
   const name = document.getElementById("name").value;
@@ -5,11 +7,15 @@ async function sendMail(event) {
   const message = document.getElementById("message").value;
   const subject = document.getElementById("subject").value;
   const phone = document.getElementById("phone").value;
+    // Check if all required fields are filled
+
   if (!name || !email || !message || !subject || !phone) {
     alert("Please fill in all fields");
     return;
   }
   try {
+        // Sending email using EmailJS
+
     await emailjs.send("service_px2iqxi", "template_pv4sf5r", {
       name,
       email,
@@ -23,11 +29,15 @@ async function sendMail(event) {
     alert("An error occurred while sending your message, please try again.");
   }
 }
+
+// Function to handle DOMContentLoaded event and initialize various elements and libraries
 document.addEventListener("DOMContentLoaded", () => {
   let e = document.querySelector("#menu-icon"),
     t = document.querySelector(".navbar"),
     a = document.querySelectorAll("section"),
     n = document.querySelectorAll("header nav a");
+      // Function to handle menu icon click event
+
   e.addEventListener("click", () => {
     e.classList.toggle("fa-xmark"), t.classList.toggle("active");
   }),
@@ -43,10 +53,14 @@ document.addEventListener("DOMContentLoaded", () => {
           l && l.classList.add("active");
         }
       }),
+          // Sticky header logic
+
         document.querySelector("header").classList.toggle("sticky", o > 100),
         e.classList.remove("fa-xmark"),
         t.classList.remove("active");
     }),
+      // Initialize ScrollReveal animations
+
     ScrollReveal({ distance: "80px", duration: 2e3, delay: 200 }).reveal(
       ".home-content, .heading, .portfolio-box",
       { origin: "top" }
@@ -59,6 +73,8 @@ document.addEventListener("DOMContentLoaded", () => {
       ".home-contact h1, .about img, .contact-img, .about-content",
       { origin: "left" }
     ),
+      // Initialize Typed.js for typing animation
+
     new Typed(".multiple-text", {
       strings: ["Frontend Developer", "Web Developer"],
       typeSpeed: 70,
@@ -67,6 +83,8 @@ document.addEventListener("DOMContentLoaded", () => {
       loop: !0,
     });
 }),
+
+// Function to handle theme switching
   (function () {
     let e = document.documentElement,
       t = document.querySelector("#theme-switcher");
@@ -77,10 +95,14 @@ document.addEventListener("DOMContentLoaded", () => {
     (n = localStorage.getItem("theme")),
       (o = window.matchMedia("(prefers-color-scheme: dark)").matches),
       a(n || (o ? "dark" : "light")),
+        // Apply the stored or default theme
+
       t.addEventListener("click", function t() {
         a("dark" === e.getAttribute("data-theme") ? "light" : "dark");
       });
   })(),
+
+// Function to initialize particles.js background animation
   particlesJS("particles-js", {
     particles: {
       number: {
@@ -113,7 +135,7 @@ document.addEventListener("DOMContentLoaded", () => {
       },
       move: {
         enable: !0,
-        speed: 6,
+        speed: 8,
         direction: "none",
         random: !1,
         straight: !1,
@@ -123,7 +145,7 @@ document.addEventListener("DOMContentLoaded", () => {
       },
     },
     interactivity: {
-      detect_on: "canvas",
+      detect_on: "scroll",
       events: {
         onhover: { enable: !0, mode: "repulse" },
         onclick: { enable: !0, mode: "push" },
@@ -139,6 +161,8 @@ document.addEventListener("DOMContentLoaded", () => {
     },
     retina_detect: !0,
   });
+
+  // Preloader logic to handle loading effect
 var m,
   divId,
   initLatitude,
@@ -154,10 +178,22 @@ $(window).on("load", function () {
         "<div class='preloader'><div><span>L</span><span>O</span><span>A</span><span>D</span><span>I</span><span>N</span><span>G</span></div></div>"
       )
     ),
+
+    // Function to handle scroll to top button click event
   document.addEventListener("DOMContentLoaded", function () {
     var e = document.querySelector(".button-top"),
       t = document.querySelector("#Home");
     e.addEventListener("click", function () {
       t && t.scrollIntoView({ behavior: "smooth" });
     });
+  });
+
+
+  window.addEventListener('scroll', function() {
+    let navbar = document.querySelector('header');
+    if (window.scrollY > 50) {
+      navbar.style.backgroundColor = 'var(--bg-color)';  
+    } else {
+      navbar.style.backgroundColor = 'transparent'; 
+    }
   });
